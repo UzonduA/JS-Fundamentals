ea712b6e430f6075936767c7af432c88723b4035
Uzondu Agbafor <nnennauzondu@gmail.com>
2025-07-13 00:10:05 +0100

    fixed a stderr with an additional line

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -8,7 +8,11 @@ const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let output = '';
 
 for (let i = 0; i < messages.length; i++) {
-  output += messages[i] + '\n';
+  result += messages[i];
 }
+
+if (i < messages.length - 1) {
+    result += '\n';
+  }
 
 console.log(output);
